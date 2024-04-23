c1e34289207af9e4ac62c4f8c5d37b2cd8d34d98
krisnayogantaraa <kyogantara89@gmail.com>
2024-04-23 19:17:58 +0700

    fix nama file

--- app/Models/menu.php
+++ app/Models/menu.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class menus extends Model
+class menu extends Model
 {
     use HasFactory;
 }
--- database/migrations/2024_04_23_121714_create_menus_table.php
+++ database/migrations/2024_04_23_121714_create_menus_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('menuses', function (Blueprint $table) {
+        Schema::create('menus', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('menuses');
+        Schema::dropIfExists('menus');
     }
 };
